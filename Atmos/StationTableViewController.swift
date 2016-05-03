de5be571b6ace4dbe181d1d8eea268814c711083
SunKyu <leesunkeuy@naver.com>
2016-05-03 16:32:01 +0900

    during implement
    check main.stroyborad and Station*.swift files

---
+++ Atmos/StationTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StationTableViewController.swift
+//  Atmos
+//
+//  Created by SunKyu Lee on 5/3/16.
+//  Copyright © 2016 SunKyu Lee. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class StationTableViewController: UITableViewController {
+    
+}
